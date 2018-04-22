4a4acc15903c68aeb26c08d5da84012f8b2e5853
JiaoCaiwei <656122978@qq.com>
2018-04-22 17:52:06 +0800

    资讯

--- js/web/info.js
+++ js/web/info.js
@@ -1,3 +1,8 @@
+$(function(){
+    $("#mainPage").load("main.html");
+    $("#footerPage").load("footer.html");
+})
+
 //banner图
 $.ajax({
     type: "post",
@@ -53,23 +58,75 @@ $.ajax({
 var pageNo = 1;
 $(document).ready(function(){
     $('.previous').click(function(){
-        pageNo -= 1;
-        reladPage();
+        if(pageNo == total){
+            $(this).next('i').show();
+            alert('这已经是第一页了');
+        }else{
+            pageNo -= 1;
+            reladPage();
+        }
     })
     $('.next').click(function(){
-        pageNo += 1;
-        reladPage();
+        //alert(total);
+        if(pageNo == total){
+            $(this).next('i').show();
+            alert('这已经是最后一页了');
+        }else{
+            pageNo += 1;
+            reladPage();
+        }
     })
 })
 function reladPage(){
     $.ajax({
         type: "post",
         url: "http://client.urbanfit.cn/message/list",
-        data:{"pageNo": pageNo,"pageSize":10},
+        data: {"pageNo": pageNo, "pageSize": 10},
         dataType: "json",
-        success:function(res){
+        success: function (res) {
             var listbox = '';
             var Page = '';
+            if (res.code == 1) {
+                var baseUel0 = res.data.baseUrl; //图片地址上前缀
+                var lstMessage = res.data.lstMessage;
+                var totalRecord = res.data.totalRecord;
+                total = Math.ceil(totalRecord/10);
+                if(total == 1){
+                    Page += '<span>'+pageNo+'</span>';
+                }else{
+                    Page += '<span>'+pageNo+'-'+total+'</span>';
+                }
+                $.each(lstMessage, function (k, v) {
+                    //alert(v.content);
+                    if (k%2 == 0) {
+                        listbox += "<li>";
+                        listbox += '<a href="'+ v.messageId + '">';
+                        listbox += '<div class="listimg">';
+                        listbox += '<img src="'+baseUel0+v.thumbnails+'">';
+                        listbox += '</div>';
+                        listbox += '<div class="listtext">';
+                        listbox += '<h1>'+v.title+'<span>'+v.createTime+'</span></h1>';
+                        listbox += '<p>'+ v.content +'</p>';
+                        listbox += '</div>';
+                        listbox += '</a>';
+                        listbox += '</li>';
+                    }else{
+                        listbox += '<li>';
+                        listbox += '<a href="' + v.messageId + '">';
+                        listbox += '<div class="listtext1">';
+                        listbox += '<h1>'+v.title+'<span>'+v.createTime+'</span></h1>';
+                        listbox += '<p>'+v.content+'</p>';
+                        listbox += '</div>';
+                        listbox += '</a>';
+                        listbox += '</li>';
+                    }
+                });
+                $("#listbox").html(listbox);
+                $("#page").html(Page);
+            } else {
+                alert("接口请求错误");
+            }
         }
     });
-}
+};
+reladPage();
