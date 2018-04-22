7734e6ab9b41a90cc518453e37a4f6df38ff006f
shishaonan <13718725223.com>
2018-04-22 16:18:09 +0800

    头部、底部公共部分提取 教练认证页面修改

--- js/web/attestion.js
+++ js/web/attestion.js
@@ -1,3 +1,32 @@
 $(function(){
-    alert("nihao")
+    $("#mainPage").load("main.html");
+    $("#footerPage").load("footer.html");
+    $("#A_query_auth").click(queryCoachAuth);
 })
+
+function queryCoachAuth(){
+    var coachName = $("input[name='coachName']").val();
+    if(coachName == ""){
+        alert("请输入教练名称");
+        return ;
+    }
+    var coachCardNum = $("input[name='coachCardNum']").val();
+    if(coachCardNum == ""){
+        alert("请输入教练认证号码");
+        return ;
+    }
+    $.ajax({
+        type : "post",
+        url : "http://client.urbanfit.cn/auth/query",
+        data : {"coachName": coachName, "coachCardNum" : coachCardNum},
+        dataType : "json",
+        success : function(result, status){
+            if(result.code != 1){
+                alert(result.msg);
+                return ;
+            }else{
+                alert("教练认证信息存在");
+            }
+        }
+    });
+}
