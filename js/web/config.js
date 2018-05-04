8ce55401bd3f3549a87cbe4e8180002953c10a6e
shishaonan <13718725223@163.com>
2018-05-04 16:41:47 +0800

    正式、测试接口地址修改

--- js/web/config.js
+++ js/web/config.js
@@ -1,2 +1,7 @@
-var env = "development";
+// 正式地址
+var env = "product";
 var baseUrl = "http://client.urbanfit.cn/";
+
+// 测试地址
+//var env = "development";
+//var baseUrl = "http://client.test.urbanfit.cn/";
